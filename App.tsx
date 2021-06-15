c24f5779d4bd134ccfeb557e074b5e0ee00987f7
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-06-15 08:41:55 -0300

    refactor: fontsLoads in App ; fix: performance when Init App

--- App.tsx
+++ App.tsx
@@ -1,31 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import AppLoading from "expo-app-loading";
-import * as Font from "expo-font";
+import { useFonts } from "expo-font";
 import { AuthProvider } from "./src/hook/authContext";
 import { ThemeProvider } from "styled-components";
 import { Routes } from "./src/routes";
 import { colors } from "./src/styles/colors";
 
 export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+  let [customFonts] = useFonts({
+    HelveticaUltraLight: require("./assets/fonts/HelveticaNeue.ttf"),
+    HelveticaThin: require("./assets/fonts/HelveticaNeueThin.ttf"),
+    HelveticaLight: require("./assets/fonts/HelveticaNeueLight.ttf"),
+    HelveticaMedium: require("./assets/fonts/HelveticaNeueMedium.ttf"),
+    HelveticaBold: require("./assets/fonts/HelveticaNeueBold.ttf"),
+    HelveticaHeavy: require("./assets/fonts/HelveticaNeueHv.ttf"),
+    HelveticaItalic: require("./assets/fonts/HelveticaNeueIt.ttf"),
+  });
 
-  const fetchFonts = async () => {
-    return Font.loadAsync({
-      HelveticaUltraLight: require("./assets/fonts/HelveticaNeue.ttf"),
-      HelveticaThin: require("./assets/fonts/HelveticaNeueThin.ttf"),
-      HelveticaLight: require("./assets/fonts/HelveticaNeueLight.ttf"),
-      HelveticaMedium: require("./assets/fonts/HelveticaNeueMedium.ttf"),
-      HelveticaBold: require("./assets/fonts/HelveticaNeueBold.ttf"),
-      HelveticaHeavy: require("./assets/fonts/HelveticaNeueHv.ttf"),
-      HelveticaItalic: require("./assets/fonts/HelveticaNeueIt.ttf"),
-    }).then(() => setFontsLoaded(true));
-  };
-
-  useEffect(() => {
-    fetchFonts();
-  }, []);
-
-  if (!fontsLoaded) return <AppLoading />;
+  if (!customFonts) return <AppLoading />;
 
   return (
     <ThemeProvider theme={{ colors }}>
